74d703977c376210a2b42d5c50cc29449dac71ad
gavlaaaaaaaa <gavlaaaaa5@googlemail.com>
2015-08-16 21:37:07 +0100

    search fragment spinners updated with array items

--- app/src/main/java/com/gav/podcaster/PodcastSearchDialogFragment.java
+++ app/src/main/java/com/gav/podcaster/PodcastSearchDialogFragment.java
@@ -6,7 +6,10 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Gav on 16/08/15.
@@ -19,7 +22,8 @@ public class PodcastSearchDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.fragment_search_dialog, null));
+        View dialogFragView = inflater.inflate(R.layout.fragment_search_dialog, null);
+        builder.setView(dialogFragView);
         //set title
         builder.setMessage("Search for Podcasts...")
                 //Set positive button text and on click listener
@@ -37,6 +41,19 @@ public class PodcastSearchDialogFragment extends DialogFragment {
                     }
                 });
 
+        //initialise spinner and array adapter for SEARCH SOURCE
+        Spinner searchSourceSpinner = (Spinner) dialogFragView.findViewById(R.id.SRCHFRAG_SPIN_SOURCE);
+        ArrayAdapter<CharSequence> srcArrayAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.search_source_array, android.R.layout.simple_spinner_item);
+        srcArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        searchSourceSpinner.setAdapter(srcArrayAdapter);
+
+        //initialise spinner and array adapter for SORT BY
+        Spinner sortSpinner = (Spinner) dialogFragView.findViewById(R.id.SRCHFRAG_SPIN_SORT);
+        ArrayAdapter<CharSequence> sortArrayAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sort_by_array, android.R.layout.simple_spinner_item);
+        sortArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sortSpinner.setAdapter(sortArrayAdapter);
 
         Dialog dialog = builder.create();
         dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
